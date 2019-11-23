363cda589a358899e2eac16dd0b8ba6e55f7e5e4
Mattia <mattia.sinisi@virgilio.it>
2019-11-23 22:05:36 +0100

    Added courses, users and schools

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\School;
+use App\Course;
+use Illuminate\Support\Facades\Auth;
+
+class CourseController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public static function create(Request $request){
+        if (!Auth::user()->school_id) return back();
+        
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        $course = new Course();
+
+        $course->title = $request->title;
+        $course->description = $request->desc;
+        $course->content = $request->content;
+        $course->author_id = Auth::user()->id;
+        $course->school_id = Auth::user()->school_id;
+
+        if($course->save()) return redirect()->route('home');
+        else return back()->withInput();
+    }
+
+    public static function list(String $school_name){
+        $school = School::where('name', $school_name)->first();
+
+        if ($school)
+           return Course::where('school_id', $school->id)->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,39 @@
 |
 */
 
+use App\Http\Controllers\CourseController;
+use App\School;
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', function(){
+    return view('searchschool');
+});
+
+Route::get('/home/{school_name}', function($school_name){
+    return view('home', [
+        "school" => School::where('name', $school_name)->first(),
+        "list" => CourseController::list($school_name)
+    ]);
+})->name('school');
+
+Route::get('/home/{school_name}/course/{course_id}', function($school_name, $course_id){
+    return view('home', [
+        "school" => School::where('name', $school_name)->first(),
+        "list" => CourseController::list($school_name)
+    ]);
+})->name('course');
+
+Route::middleware('auth')->group(function(){
+    Route::prefix('/new')->group(function(){
+        Route::get('course', 'PostController@coursePage');
+    });
+
+    Route::prefix('/publish')->group(function(){
+        Route::post('course', 'CourseController@create');
+    });
+});
