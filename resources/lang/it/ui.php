f66ee62308f2bf657e75d0562b997b1769184d68
Mattia <mattia.sinisi@virgilio.it>
2019-12-04 17:55:42 +0100

    Italian UI translation

---
+++ resources/lang/it/ui.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    "login" => "Accedi",
+    "register" => "Registrati",
+    "logout" => "Esci",
+    "create" => "Crea",
+    "course" => "Corso",
+    "activity" => "Attività",
+    "new" => [
+        "course" => "Nuovo corso",
+        "activity" => "Consiglia attività"
+    ],
+    "subscribe" => "Iscriviti",
+    "admin" => [
+        "panel" => "Pannello admin"
+    ],
+    "explore" => [
+        "generic" => "Esplora",
+        "schools" => "Esplora le scuole"
+    ],
+    "discover" => [
+        "generic" => "Scopri",
+        "schools" => "Scopri le scuole"
+    ],
+    "search" => [
+        "generic" => "Cerca",
+        "schools" => "Ricerca le scuole"
+    ],
+    "return" => [
+        "generic" => "Indietro",
+        "to" => [
+            "home" => "Ritorna alla home"
+        ]
+    ],
+    "schools" => "Scuole",
+    "sorry" => "Ops",
+    "publish" => "Pubblica",
+    "title" => "Titolo",
+    "description" => "Descrizione",
+    "content" => "Contenuto",
+    "date" => "Data",
+    "submit" => "Invia",
+    "your" => [
+        "school" => "La tua scuola",
+        "profile" => "Il tuo profilo"
+    ],
+    "home" => "Home",
+];
