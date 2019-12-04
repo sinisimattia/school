ae64b47f6b33b1799a698b86bcf0060616934d36
Mattia <mattia.sinisi@virgilio.it>
2019-12-04 17:56:11 +0100

    Italian translation for errors

---
+++ resources/lang/it/errors.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "error" => "Errore",
+    "this" => [
+        "course" => [
+            "inexistent" => "Questo corso non esiste"
+        ],
+        "user" => [
+            "inexistent" => "Questo utente non esiste"
+        ]
+    ]
+];
