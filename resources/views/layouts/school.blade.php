e0a19f9055a3a8f27a3bee64d94f769230ff5b76
Mattia <mattia.sinisi@virgilio.it>
2019-12-08 21:29:07 +0100

    School name is smaller

--- resources/views/layouts/school.blade.php
+++ resources/views/layouts/school.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="hero is-primary is-small">
     <div class="hero-body">
-        <h1 class="title is-1 has-text-centered">{{$school->fullname}}</h1>
+        <h1 class="title is-3 has-text-centered">{{$school->fullname}}</h1>
     </div>
 </div>
 
